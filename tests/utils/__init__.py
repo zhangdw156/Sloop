92e5d635cbf85b3062fd0c0d494f4bb9b864faa4
zhangdw156 <zhangdawei24@mails.ucas.ac.cn>
2026-01-02 01:27:28 +0800

    refactor: rename test_logger.py to logger.py for better naming

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -2,6 +2,6 @@
 测试工具模块
 """
 
-from .test_logger import get_test_logger, get_current_test_logger
+from .logger import get_test_logger, get_current_test_logger
 
 __all__ = ["get_test_logger", "get_current_test_logger"]
