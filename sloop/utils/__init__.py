eed38f9bf5bec46d97510a934a7bc49bc078f8e4
zhangdw <zhangdw.cs@gmail.com>
2026-01-09 17:53:04 +0800

    feat: complete tools->graph->skeletons->intents

--- sloop/utils/__init__.py
+++ sloop/utils/__init__.py
@@ -4,7 +4,7 @@
 导出各种实用工具函数。
 """
 
-from sloop.utils.graph import GraphBuilder
+from sloop.utils.graph_builder import GraphBuilder
 from sloop.utils.graph_sampler import GraphSampler
 from sloop.utils.logger import logger, setup_logging
 from sloop.utils.intent_generator import IntentGenerator
